dbf7e6dea496beb411638f290e0bd03b74e98ce7
kaitlyn blanton <kaitlynb.04@icloud.com>
2026-08-07 07:27:10 -0600

    add basic js structure

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const yosemiteValley = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+const lakeLouise = {
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+
+const baldMountains = {
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+
+const latemar = {
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+
+const vanoise = {
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+
+const lago = {
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
+
+let initialCards = [
+  yosemiteValley,
+  lakeLouise,
+  baldMountains,
+  latemar,
+  vanoise,
+  lago,
+];
